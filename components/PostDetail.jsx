6447e64fcb8713bfb852e437ed0663e800bdd9f5
Nate <85075459+estin8@users.noreply.github.com>
2021-11-06 15:28:27 -0400

    css fixes

--- components/PostDetail.jsx
+++ components/PostDetail.jsx
@@ -60,7 +60,7 @@ const PostDetail = ({ post }) => {
   };
 
   return (
-    <div className="bg-white shadow-lg rounded-lg lg:p-8 pb-12 mb-8">
+    <div className="bg-white shadow-lg rounded-t-lg lg:p-8 pb-12 mb-8">
       <div className="relative overflow-hidden shadow-md mb-6">
         <img
           src={post.featuredImage.url}
